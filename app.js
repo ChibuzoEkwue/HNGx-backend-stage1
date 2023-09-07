963645b43e76731e74a631d81729e49d3174ebb1
Chibuzo Ekwue <chieezy5@gmail.com>
2023-09-07 21:27:16 +0100

    minor adjustment

--- app.js
+++ app.js
@@ -20,14 +20,19 @@ app.get("/", (req, res) => {
 	];
 
 	const current_day = days[date.getDay()];
-    
+
 	res.json({
-		slack_name,
+		slack_name:
+			slack_name ||
+			"Enter your slackname by passing it as a value to the query params slack_name",
 		current_day,
 		utc_time: date,
-		track,
-		github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
-		github_repo_url: "https://github.com/username/repo",
+		track:
+			track ||
+			"Enter your track by passing it as a value to the query params track",
+		github_file_url:
+			"https://github.com/ChibuzoEkwue/HNGx-backend-stage1/blob/main/app.js",
+		github_repo_url: "https://github.com/ChibuzoEkwue/HNGx-backend-stage1",
 		status_code: 200,
 	});
 });
